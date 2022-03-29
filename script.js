1cb8e518e95e647012b97f8d273bdd3134d05b93
Beto1821 <beto1821@uol.com.br>
2022-03-28 22:18:19 -0300

    ex07 rev00

--- script.js
+++ script.js
@@ -3,14 +3,26 @@ const cartaGerada = document.getElementById('carta-gerada');
 function geraCarta() {
   const entrada = document.getElementById('carta-texto');
   const texto = entrada.value;
-  const array = texto.split(' ');
+  const lista = texto.split(' ');
+  console.log(lista.lenght);
+  console.log(lista);
   cartaGerada.innerHTML = '';
-  for (let i = 0 ; i < array.length; i += 1) {
+  for (let i = 0; i < lista.length; i += 1) {
     const lineSpam = document.createElement('span');
-    lineSpam.innerText = array[i];
+    lineSpam.innerText = lista[i];
     cartaGerada.appendChild(lineSpam);
   }
 }
 
+function validaInput() {
+  const entrada = document.getElementById('carta-texto');
+  const texto = entrada.value;
+  if (texto < 1) {
+    cartaGerada.innerText = 'Por favor, digite o conteúdo da carta.';
+  } else {
+    geraCarta();
+  }
+}
+
 const btn = document.getElementById('criar-carta');
-btn.addEventListener('click', geraCarta);
+btn.addEventListener('click', validaInput);
